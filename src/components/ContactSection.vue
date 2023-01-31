b433bd4109565949bd041be048931e38bf17c08b
VittorioRossi <rossi.vittori@gmail.com>
2023-01-31 02:59:29 +0100

    contact update

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -6,50 +6,55 @@
         </div>
 
         <div class="content">
-            
-            <lord-icon
-                src="https://cdn.lordicon.com/rhvddzym.json"
-                trigger="loop"
-                delay="2000"
-                colors="primary:#00EE01,secondary:#00EE01"
-                style="width:250px;height:250px;">
-            </lord-icon>
 
-            <div class="col-1">
-                <h4>
-                    Contacts
-                </h4>
-                <a class = "link" href="mailto:rossi.vittori@gmail.com">rossi.vittori@gmail.com</a>    
+            <form action="https://formsubmit.co/rossi.vittori@gmail.com" method="POST">
+                <input class="input-field" type="text" name="name" id="name" required>
+                <div class="label-div">
+                    <label for="name">Name</label>
+                </div>
+                <input class="input-field" type="email" name="email" id="email" required>
+                <div class="label-div">
+                    <label for="email">E-mail</label>
+                </div>
+                <textarea class="input-field" name="content" cols="20" rows="10" id="content" required></textarea>
+                <div class="label-div">
+                    <label for="content">Your message</label>
+                </div>
                 
 
-            </div>
-            <div class="col-2">
-                <h4>
-                    My profiles
-                </h4>
-                <a class = "link" href="https://www.linkedin.com/in/vittorio-rossi-645749226/">Linkedin</a>
-                <a class = "link" href="https://github.com/VittorioRossi">GitHub</a>
-                <a class = "link" href="https://www.kaggle.com/vittoriorossi">Kaggle</a>
-                <a class = "link" href="https://www.fiverr.com/vittoriorossi23">Fiverr</a>
+                <div class="send-btn">
+                    <button type="submit" class="btn">Send</button>
 
-            </div>
-
-            <div class="col-3">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/rhvddzym.json"
+                        trigger="loop"
+                        delay="2000"
+                        colors="primary:#00EE01,secondary:#00EE01"
+                        style="width:50px;height:50px;">
+                    </lord-icon>
+                </div>
+            </form>
+            
+            <div class="other-links">
                 <h4>
-                    Downloads
+                    Other links
                 </h4>
-                <a class = "link-btn" href="./src/assets/file/CV-EN.pdf" download>
+                <a class = "btn" href="https://www.linkedin.com/in/vittorio-rossi-645749226/">Linkedin</a>
+                <a class = "btn" href="https://github.com/VittorioRossi">GitHub</a>
+                <a class = "btn" href="https://www.kaggle.com/vittoriorossi">Kaggle</a>
+                <a class = "btn" href="https://www.fiverr.com/vittoriorossi23">Fiverr</a>
+
+                <a class = "download__btn" href="./src/assets/file/CV-EN.pdf" download>
                     <div class="btn">
                     Download my CV - EN
                     </div>
                 </a>
-                <a class = "link-btn" href="./src/assets/file/CV-EN.pdf" download>
+                <a class = "download__btn" href="./src/assets/file/CV-EN.pdf" download>
                     <div class="btn">
                     Download my CV - IT
                     </div>
                 </a>
-            </div>
-
+        </div>
         </div>
     </div>
 </template>
@@ -67,51 +72,55 @@ export default {
 .contacts {
     display: flex;
     flex-direction: column;
-
-    height: 100vh;
     overflow-x: none;
 }
 
 .content {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-
-}
-.content div {
-    height: 80%;
     display: flex;
     flex-direction: column;
-
-    margin: 20px;
+    align-items:flex-start;
 }
-.content a.link{
-    font-size: 18px;
-    font-weight: 200;
+
+form {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.input-field {
+    font-family: Nunito, -apple-system, BlinkMacSystemFont, sans-serif;;
+
+    border: 0;
+    outline: 0;
+    border-bottom: 2px solid var(--color-text);
     color: var(--color-text);
-    text-decoration: none;
-    transition: all 0.5;
+    background-color: transparent;
 
-    margin: 10px 0px;
+    transition: border-color 0.2s;
 }
 
-.content a.link:hover{
-    color: var(--color-third);
-    cursor: pointer;
-
+.input-field:focus{
+    border-color: var(--color-third);
 }
-.content a {
-    text-decoration: none;
-    color: var(--color-text);
-    margin: 0;
-    padding: 0;
+.input-field:placeholder-shown ~ label{
+    cursor: text;
+    top: 20px;
 }
 
-.col-1,.col-2 {
-    border-right: 1px solid var(--color-third);
+.label-div {
+    position: relative;
 }
 
+label {
+  position: absolute;
+  top: -55px;
+  transition: 0.2s;
+  font-size: 20px;
+  color: var(--color-secondary);
+}
 
-.btn {
+.download__btn {
     text-align: center;
     justify-content: center;
     margin: 10px 0px  0px !important;
@@ -120,6 +129,11 @@ export default {
     height: 15px;
     font-size: 15px;
 
+}
+
+.send-btn {
+    display: flex;
+    flex-direction: row;
 }
 
 </style>
